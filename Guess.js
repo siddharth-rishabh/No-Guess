3ed38f37c693de6a5dc145dc5e52fb56aaf12a8a
Siddharth <siddharthrishabh019@gmail.com>
2024-12-24 01:53:06 +0530

    updated for clearing input box

--- Guess.js
+++ Guess.js
@@ -1,17 +1,18 @@
-let findNo= 6;
+let findNo= 19;
 let outputDiv= document.getElementById("output");
+let inputField = document.getElementById("inputGuess");
 console.log(" Start the game !!");
 
 let submitGuess = document.getElementById("submitGuess");
 submitGuess.addEventListener("click", ()=>{
     let inputNum= parseInt(document.getElementById("inputGuess").value);
     if(inputNum === findNo){
-            outputDiv.innerHTML= "congratulation baby finally guessed it right";
+            outputDiv.innerHTML= "congratulation baby you finally guessed it right";
             
         }else{
             outputDiv.innerHTML= "You are close try again";
             
         }
-        outputDiv.value= "";
+        inputField.value = "";
     }
 )
