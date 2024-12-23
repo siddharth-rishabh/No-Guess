38b29ae4528fd231369354c43517b4e76ab3eed5
Siddharth <siddharthrishabh019@gmail.com>
2024-12-24 01:12:44 +0530

    Initial commit of Guess The Number Game

---
+++ Guess.js
@@ -0,0 +1,16 @@
+let findNo= 6;
+let outputDiv= document.getElementById("output");
+console.log(" Start the game !!");
+
+let submitGuess = document.getElementById("submitGuess");
+submitGuess.addEventListener("click", ()=>{
+    let inputNum= parseInt(document.getElementById("inputGuess").value);
+    if(inputNum === findNo){
+            outputDiv.innerHTML= "congratulation baby finally guessed it right";
+            
+        }else{
+            outputDiv.innerHTML= "You are close try again";
+            
+        }
+}
+)
