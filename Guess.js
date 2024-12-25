a0592e3792e2a6ee7957183d0073aff42f5d7da2
Siddharth <siddharthrishabh019@gmail.com>
2024-12-25 22:41:06 +0530

    updated code

--- Guess.js
+++ Guess.js
@@ -1,18 +1,41 @@
-let findNo= 19;
+let findNo= 5;
 let outputDiv= document.getElementById("output");
 let inputField = document.getElementById("inputGuess");
 console.log(" Start the game !!");
 
-let submitGuess = document.getElementById("submitGuess");
-submitGuess.addEventListener("click", ()=>{
+function handleGuess(){
     let inputNum= parseInt(document.getElementById("inputGuess").value);
     if(inputNum === findNo){
-            outputDiv.innerHTML= "congratulation baby you finally guessed it right";
-            
-        }else{
-            outputDiv.innerHTML= "You are close try again";
+            outputDiv.innerHTML= "congratulation 🎉🎉 baby you finally guessed it right";
             
         }
+        else if(inputNum > findNo){
+            outputDiv.innerHTML= "The number is smaller than your guess.";
+        }
+        else{
+            outputDiv.innerHTML= "The number is greater than your guess.";
+        }
+        
         inputField.value = "";
+        if(inputNum > 10){
+            outputDiv.innerHTML="🔴Please enter number less than or equal to 10"
+        }
+}
+
+let submitGuess = document.getElementById("submitGuess");
+submitGuess.addEventListener("click", ()=>{
+    handleGuess();
     }
-)
+    
+);
+inputGuess.addEventListener("keydown", (event)=>{
+    if(event.key === "Enter"){
+        handleGuess();
+    }
+});
+
+let themeChange= document.getElementById("themeToggle");
+themeChange.addEventListener("click", ()=>{
+    
+})
+
